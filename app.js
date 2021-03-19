6419ff7f2aa218416c6732cc18b8fac2a5ba8fd5
canberk <canberkk94@gmail.com>
2021-03-19 21:22:37 +0300

    without min-max ai player

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const gameBoard = (()=>{
         const getTurn = () => turn;
         const setTurn = (newTurn) => turn = newTurn;
         const play = () =>  {
-            let emptyBoard = [];
+            const emptyBoard = [];
             for(let i = 0; i<boards.length; i++){
                 if(boards[i] == undefined){
                     emptyBoard.push(i);
@@ -143,43 +143,85 @@ const gameBoard = (()=>{
 
     const play = (e) =>{
         let isEnd = checkResult.isEnd();
+        console.log(`is end: ${isEnd}`);
+        console.log(`player turn: ${player.getTurn()}`);
+        console.log(`target innerText: ${e.target.innerText}`);
         if(e.target.innerText == "" && player.getTurn() && !isEnd){
                 player.play(e);
                 player.setTurn(false);
+                isEnd = checkResult.isEnd();
        }
         if(ai.getTurn() && !isEnd){
             ai.play();
             ai.setTurn(false);
         }
         display(boards);
+        console.log(boards);
     }
 
     const endGame = () =>{
         if(checkResult.isEnd()){
-            let winner;
-            if(player.isWinner){
-                winner = "player";
-            }else if(ai.isWinner){
-                winner = "ai";
-            }
-            console.log(`Game is over: ${checkResult.isEnd()} and the Winner: ${winner}`);
+            let winnerButtons = Array.from(buttons);
+            winnerButtons = winnerButtons.filter(button => {
+                if(player.isWinner){
+                    return button.innerText == "X";
+                }else{
+                    return button.innerText == "O";
+                }
+            });
+            winnerButtons.forEach(button => button.style.fontSize = "9rem");
+            window.setTimeout(function(){
+                endGameDisplayText.innerText = (player.isWinner) ? "You Win !" : "You Lose :(";
+                endGameDisplayText.style.color = (player.isWinner) ? "rgb(24, 32, 41)" : "rgb(60, 22, 22)";
+                endGameDisplay.classList.add('end-game-declare-trigger');
+                container.classList.add('end-game-container-move');
+            }, 1000);
         }else if(checkResult.isLastRound()){
-            console.log('game is over and the result is tie');
+            window.setTimeout(function(){
+                endGameDisplayText.innerText = "Tie !";
+                endGameDisplayText.style.color = "rgb(196, 215, 236)";
+                endGameDisplay.classList.add('end-game-declare-trigger');
+                container.classList.add('end-game-container-move');
+            }, 1000);
         }
+        console.log('aa');
     };
+
+    const resetGame = ()=>{
+        for(let i = 0; i<boards.length; i++){boards[i]=undefined}
+        endGameDisplay.classList.remove('end-game-declare-trigger');
+        container.classList.remove('end-game-container-move');
+        buttons.forEach(button =>{
+            button.classList.remove('player-section');
+            button.classList.remove('computer-section');
+            button.style.fontSize = "";
+        });
+        player.setTurn(true);
+        player.isWinner = false;
+        ai.setTurn(false);
+        ai.isWinner = false; 
+        console.log(boards)
+        display(boards);
+    }
+
 
     const player = human();
     const ai = computer();
     const buttons = document.querySelectorAll('.board');
+    const againButton = document.querySelector('#again-button');
+    const endGameDisplay = document.querySelector('#end-game-declare');
+    const endGameDisplayText = document.querySelector('#end-game-declare-text');
+    const container = document.querySelector('#container');
     buttons.forEach(button => button.addEventListener('click', play));
     buttons.forEach(button => button.addEventListener('click', endGame));
+    againButton.addEventListener('click',resetGame);
+
+
+    
 })();
 
 /*
-1- Add div that declares winner and blur the body of the page.
-2- Add restart button on that div which resets the game
-3- Add style to the page
-4- Implement max-min for real ai player.
+Implement max-min for real ai player.
 */
 
 
